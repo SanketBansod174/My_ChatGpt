afa192325405e139639afcf61243c73197cfe0dd
SanketBansod174 <sanketbansod23@gmail.com>
2025-05-25 22:43:20 +0530

    minor

--- My-AI-Chat/src/app/app.component.ts
+++ My-AI-Chat/src/app/app.component.ts
@@ -2,10 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ChatComponent } from "./features/chat/chat.component";
 import { ChatSidebarComponent } from "./features/chat-sidebar/chat-sidebar.component";
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
-  imports: [ ChatComponent, ChatSidebarComponent],
+  imports: [ChatComponent, ChatSidebarComponent, MatSidenavModule, MatToolbarModule, MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
