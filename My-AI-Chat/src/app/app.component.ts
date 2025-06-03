29b91a554f3b4f4045266a89ee5cd39d3c5008c9
SanketBansod174 <sanketbansod23@gmail.com>
2025-06-03 22:25:25 +0530

    minor

--- My-AI-Chat/src/app/app.component.ts
+++ My-AI-Chat/src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ChatComponent } from "./features/chat/chat.component";
 import { ChatSidebarComponent } from "./features/chat-sidebar/chat-sidebar.component";
+import { ToolbarComponent } from "./features/toolbar/toolbar.component";
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
-  imports: [ChatComponent, ChatSidebarComponent, MatSidenavModule, MatToolbarModule, MatIconModule],
+  imports: [ChatComponent, ChatSidebarComponent, ToolbarComponent, MatSidenavModule, MatToolbarModule, MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ My-AI-Chat/src/app/features/toolbar/toolbar.component.ts
@@ -0,0 +1,60 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSelectModule } from '@angular/material/select';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { ChatService } from '../../services/chat.service';
+import { OllamaService } from '../../services/ollama.service';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { ConfirmDialogComponent } from '../../shared/confirm-dialog/confirm-dialog.component';
+
+@Component({
+  selector: 'app-toolbar',
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatSelectModule,
+    MatIconModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatDialogModule,
+    FormsModule
+  ],
+  templateUrl: './toolbar.component.html',
+  styleUrl: './toolbar.component.css'
+})
+export class ToolbarComponent {
+  @Input() selectedModel: string = '';
+  @Input() availableModels: any[] = [];
+  messages: { sender: 'user' | 'ai', text: string }[] = [];
+
+  @Output() selectedModelChange = new EventEmitter<string>();
+  @Output() clearChatClick = new EventEmitter<void>();
+
+
+  constructor(
+    private chatService: ChatService,
+    private ollamaService: OllamaService,
+    private dialog: MatDialog
+  ) { }
+
+  ngOnInit() {
+    this.messages = this.chatService.getMessages();
+    this.availableModels = this.chatService.getModels();
+    this.selectedModel = this.chatService.getSelectedModel();
+  }
+
+  changeModel() {
+    this.selectedModelChange.emit(this.selectedModel);
+  }
+
+  clearChat() {
+    this.clearChatClick.emit();
+  }
+}
